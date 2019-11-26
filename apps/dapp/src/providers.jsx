9d487511f08abfb1bf34e79ce91e0bb348fb1287
Kames <info@kamescg.com>
2019-11-26 12:52:09 -0500

    contracts

--- apps/dapp/src/providers.jsx
+++ apps/dapp/src/providers.jsx
@@ -1,24 +1,25 @@
 /* --- Global --- */
 import {ThemeProvider} from 'theme-ui';
-import {PortalProvider, PortalTree} from 'react-portal-system';
 import {BoxProvider} from '3box-ui-system';
 import {EthersProvider} from 'ethers-react-system';
+import {PortalProvider, PortalTree} from 'react-portal-system';
 
 /* --- Local --- */
 import theme from './assets/theme';
 
 const BoxConfig = {isAutoEnable: false, isAutoLogin: false};
 
+/* --- Component --- */
 export default props => {
   return (
     <ThemeProvider theme={theme}>
       <EthersProvider>
-        {/* <BoxProvider config={BoxConfig}> */}
-        <PortalProvider>
-          <PortalTree />
-          {props.children}
-        </PortalProvider>
-        {/* </BoxProvider> */}
+        <BoxProvider config={BoxConfig}>
+          <PortalProvider>
+            <PortalTree />
+            {props.children}
+          </PortalProvider>
+        </BoxProvider>
       </EthersProvider>
     </ThemeProvider>
   );
