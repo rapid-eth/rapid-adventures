4f79dfb6634cf25523ac0614e7f370fe5f717842
Kames <info@kamescg.com>
2019-12-03 10:02:48 -0800

    add Verify Event form for developers to manually test inputs

--- apps/dapp/src/components/forms/testing/fetch.js
+++ apps/dapp/src/components/forms/testing/fetch.js
@@ -2,7 +2,7 @@ export async function postData(url = '', data = {}) {
 	// Default options are marked with *
 	const response = await fetch(url, {
 		method: 'POST', // *GET, POST, PUT, DELETE, etc.
-		mode: 'cors', // no-cors, *cors, same-origin
+		mode: 'no-cors', // no-cors, *cors, same-origin
 		cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
 		credentials: 'same-origin', // include, *same-origin, omit
 		headers: {
