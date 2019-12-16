6d94b19a77861c6148ca72d6e306bb2b8885d8ac
Pat <me@onpaws.com>
2019-12-16 23:23:46 +0100

    Updates

---
+++ precache-manifest.9a96744c8b27411f0bb039a10376ac5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8568ba4173e42c8d5525d7e9ca508cff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2796bec777ff3540d3c2",
+    "url": "./static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "81ce971961c1668885c6",
+    "url": "./static/css/main.27bace45.chunk.css"
+  },
+  {
+    "revision": "2796bec777ff3540d3c2",
+    "url": "./static/js/2.e14a559b.chunk.js"
+  },
+  {
+    "revision": "81ce971961c1668885c6",
+    "url": "./static/js/main.66c44f41.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0489480e48e470a0d819ffacf5ed02fc.js"
+  "./precache-manifest.9a96744c8b27411f0bb039a10376ac5f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
