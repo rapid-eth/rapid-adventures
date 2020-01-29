db4c5f85041c7456f3a786e9d234efbaf233c807
Pat <me@onpaws.com>
2020-01-29 02:10:21 +0100

    wip

--- apps/dapp/src/pages/MUI/Adventures.jsx
+++ apps/dapp/src/pages/MUI/Adventures.jsx
@@ -4,6 +4,7 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { Typography, Button, AppBar, Toolbar, InputBase } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import rapidLogo from '../../assets/rapid.svg'
 
 import adventures from 'data/adventureList.json';
@@ -30,12 +31,22 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: '#E9E9E9',
     padding: 18,
   },
-  card: {
+  adventureCard: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#FFFFFF',
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    marginBottom: theme.spacing(10)
+    marginBottom: theme.spacing(10),
+    position: 'relative'
+  },
+  adventureCardButton: {
+    position: 'absolute',
+    bottom: -18,
+    right: 25
   },
   selected: {
     outline: '4px solid #5436D6',
@@ -84,6 +95,32 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
+  questCard: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'relative',
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.common.white,
+    borderRadius: 4,
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(15)
+  },
+  questCardHeader: {
+    position: 'absolute',
+    top: -41,
+    right: 25,
+    width: '50%',
+    display: 'flex',
+    backgroundColor: theme.palette.common.white,
+    padding: theme.spacing(1),
+    borderBottom: '1px solid gray',
+    borderRadius: '4px 4px 0 0'
+  },
+  questCardButtonContainer: {
+    position: 'absolute',
+    bottom: -18,
+    right: 25,
+  }
 }));
 
 const AdventuresPage = () => {
@@ -91,17 +128,15 @@ const AdventuresPage = () => {
   const classes = useStyles();
   const [selectedAdventureId, selectAdventure] = useState()
   const adventure = adventures.data.find(({ id }) => id === selectedAdventureId)
-  const [questSearch, setQuestSearch] = useState()
   const inputEl = useRef(null);
 
-  const handleChange = (event) => {
-    setQuestSearch(event.target.value)
-  }
+  // const { quests: theQuests } = adventure;
+  // const filteredQuests = theQuests.length && theQuests.filter((quest) => {
+  //   return quest.match(/ /)
+  // })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(inputEl.current);
-    console.log(inputEl.current.value);
   }
 
   return (
@@ -111,10 +146,13 @@ const AdventuresPage = () => {
         <div className={classes.wide}>
           {
             adventures.data.map(({ id, title, subtitle, image, alias }) =>
-              <div key={id} className={clsx(classes.card, selectedAdventureId === id && classes.selected)} onClick={() => selectAdventure(id)}>
+              <div key={id} className={clsx(classes.adventureCard, selectedAdventureId === id && classes.selected)} onClick={() => selectAdventure(id)}>
                 <img src={rapidLogo} alt="rapid logo" />
                 <br />
                 {title}
+                <Button size="small" variant="contained" color="primary" className={classes.adventureCardButton}>
+                  <AddCircleIcon />&nbsp; Explore
+                </Button>
               </div>
             )
           }
@@ -144,7 +182,28 @@ const AdventuresPage = () => {
         </Toolbar>
         {selectedAdventureId &&
           <div className={classes.quests}>
-            {adventure.quests.map(({ id, properties: { title } }) => <div key={id}>{title}</div>)}
+            {adventure.quests.map(({ id, properties: { title, subtitle } }) =>
+              <div key={id} className={classes.questCard}>
+                <header className={classes.questCardHeader}>
+                  <Typography>Difficulty: 1</Typography>
+                  &nbsp;&nbsp;
+                  <Typography>Estimated time: 123</Typography>
+                  &nbsp;&nbsp;
+                  <Typography>Reward: 5 ETH</Typography>
+                </header>
+                <Typography>{title}</Typography>
+                <Typography>{subtitle}</Typography>
+                <div className={classes.questCardButtonContainer}>
+                  <Button size="small" variant="contained" color="primary">
+                    More Info
+                  </Button>
+                  &nbsp; &nbsp;
+                  <Button size="small" variant="contained" color="primary">
+                    Start Quest
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
         }
       </div>
